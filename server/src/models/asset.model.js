4d95bd30a602f38689da1cdb08017b965c849bb9
tenjinchaney <jimmy981103@apps.ntpc.edu.tw>
2026-07-29 18:05:36 +0800

    feat(asset): add asset model

---
+++ server/src/models/asset.model.js
@@ -0,0 +1,10 @@
+class Asset {
+  constructor({ id, name, category, status }) {
+    this.id = id;
+    this.name = name;
+    this.category = category;
+    this.status = status;
+  }
+}
+
+module.exports = Asset;
