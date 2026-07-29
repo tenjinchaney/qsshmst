6ca5e7dfbfef593ce4405ff54367cda3e1bb3713
tenjinchaney <jimmy981103@apps.ntpc.edu.tw>
2026-07-29 18:39:40 +0800

    feat(ui): add sidebar component

---
+++ client/src/components/Sidebar.jsx
@@ -0,0 +1,14 @@
+function Sidebar() {
+  return (
+    <aside>
+      <ul>
+        <li>Dashboard</li>
+        <li>Assets</li>
+        <li>Borrow / Return</li>
+        <li>Reports</li>
+      </ul>
+    </aside>
+  );
+}
+
+export default Sidebar;
